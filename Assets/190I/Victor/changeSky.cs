62ac4ccbcd335ff70170e0cf7efd758a19143351
Victor <vhg@ucsb.edu>
2024-05-22 14:08:51 -0700

    removed ground

--- Assets/190I/Victor/changeSky.cs
+++ Assets/190I/Victor/changeSky.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class changeSky : MonoBehaviour
 {
     public Material mat1;
+    // Names of the objects to be removed
+    private string[] objectNames = new string[]
+    {
+        "Geo_StandardStage",
+        "Geo_FloorBackground",
+        "Geo_FloorForeground",
+        "Geo_Spikes",
+        "GroundDisk"
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +27,22 @@ public class changeSky : MonoBehaviour
     { 
         mat1.SetColor("_Tint", Color.grey);
         RenderSettings.skybox = mat1;
+        // Loop through each object name
+        foreach (string objectName in objectNames)
+        {
+            // Find the GameObject by name
+            GameObject obj = GameObject.Find(objectName);
+
+            // If the object is found, destroy it
+            if (obj != null)
+            {
+                Destroy(obj);
+              //  Debug.Log(objectName + " has been removed from the scene.");
+            }
+            else
+            {
+               // Debug.LogWarning(objectName + " could not be found in the scene.");
+            }
+        }
     }
 }
